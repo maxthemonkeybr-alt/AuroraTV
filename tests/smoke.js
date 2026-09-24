23231f111f3037dd5b281e6d102d75e335d23633
maxthemonkeybr-alt <maxthemonkeybr@gmail.com>
2026-09-23 22:37:17 -0300

    Update Aurora TV smoke tests

--- tests/smoke.js
+++ tests/smoke.js
@@ -1,9 +1,10 @@
 "use strict";
-var fs=require("fs"),path=require("path"),root=path.resolve(__dirname,".."),required=["package.json","index.html","style.css","core.js","xtream.js","mock.js","player.js","app.js","dev-server.js"],fail=[];
+var fs=require("fs"),path=require("path"),root=path.resolve(__dirname,".."),required=["package.json","index.html","style.css","core.js","xtream.js","mock.js","player.js","app.js","service.js","dev-server.js"],fail=[];
 required.forEach(function(f){if(!fs.existsSync(path.join(root,f)))fail.push("Arquivo ausente: "+f)});
-var pkg=JSON.parse(fs.readFileSync(path.join(root,"package.json"),"utf8"));if(pkg.packageType!=="app")fail.push("packageType deve ser app");if(pkg.appPath!=="index.html")fail.push("appPath incorreto");if(pkg.appName!=="Aurora TV")fail.push("appName incorreto");
-var html=fs.readFileSync(path.join(root,"index.html"),"utf8"),css=fs.readFileSync(path.join(root,"style.css"),"utf8"),app=fs.readFileSync(path.join(root,"app.js"),"utf8"),player=fs.readFileSync(path.join(root,"player.js"),"utf8");
+var pkg=JSON.parse(fs.readFileSync(path.join(root,"package.json"),"utf8"));if(pkg.packageType!=="app")fail.push("packageType deve ser app");if(pkg.appPath!=="index.html")fail.push("appPath incorreto");if(pkg.appName!=="Aurora TV")fail.push("appName incorreto");if(pkg.serviceFile!=="service.js")fail.push("serviceFile ausente/incorreto");
+var html=fs.readFileSync(path.join(root,"index.html"),"utf8"),css=fs.readFileSync(path.join(root,"style.css"),"utf8"),app=fs.readFileSync(path.join(root,"app.js"),"utf8"),player=fs.readFileSync(path.join(root,"player.js"),"utf8"),xtream=fs.readFileSync(path.join(root,"xtream.js"),"utf8"),service=fs.readFileSync(path.join(root,"service.js"),"utf8");
 ["serverInput","mainNav","playerOverlay","aspectMenu","video"].forEach(function(x){if(html.indexOf('id="'+x+'"')<0)fail.push("Elemento ausente: "+x)});
 ["original","auto","letterbox","fill","16:9","4:3","zoom","reset"].forEach(function(m){if(html.indexOf('data-aspect="'+m+'"')<0)fail.push("Modo de imagem ausente: "+m)});
 if(app.indexOf("seriesInfo")<0||app.indexOf("continueItems")<0)fail.push("Integração de séries/continuar assistindo incompleta");if(player.indexOf("saveProgress")<0||player.indexOf("setInterval(persist,5000)")<0)fail.push("Persistência do player incompleta");if(css.indexOf(".mode-43")<0||css.indexOf(".mode-zoom")<0)fail.push("CSS dos formatos de imagem incompleto");
-if(fail.length){console.error("FALHOU\n- "+fail.join("\n- "));process.exit(1)}console.log("OK - Aurora TV passou nos testes estruturais.");
+if(xtream.indexOf("127.0.0.1:18081/proxy")<0)fail.push("Proxy Xtream da TV ausente");if(service.indexOf('server.listen(PORT,"127.0.0.1"')<0)fail.push("Serviço proxy local ausente");
+if(fail.length){console.error("FALHOU\n- "+fail.join("\n- "));process.exit(1)}console.log("OK - Aurora TV v"+pkg.version+" passou nos testes estruturais.");
